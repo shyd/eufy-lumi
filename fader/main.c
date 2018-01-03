dafec51c1b435dc43a598e800cdfeff6012aa632
Dennis <dennis@shyd.de>
2018-01-03 20:57:49 +0100

    initial commit, empty project

---
+++ fader/main.c
@@ -0,0 +1,18 @@
+/*
+ * fader.c
+ *
+ * Created: 03.01.2018 20:55:13
+ * Author : Dennis
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
