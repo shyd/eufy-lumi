da9013b1a080e66fcea71f2b83dbf6b07d127019
Dennis Schütt <dennis@shyd.de>
2018-02-21 20:14:45 +0100

    cleanup

--- fader/main.c
+++ fader/main.c
@@ -2,8 +2,8 @@
  * fader.c
  *
  * Created: 03.01.2018 20:55:13
- * Author : Dennis
- */ 
+ * Author : Dennis Schuett <https://schuett.io>
+ */
 
 #define F_CPU 1000000UL
 
@@ -13,30 +13,19 @@
 
 int main(void)
 {
-	// Set CPU speed by setting clock prescaler:
-	// CCP register must first be written with the correct signature - 0xD8
-	//CCP = 0xD8;
-	//  CLKPS[3:0] sets the clock division factor
-	//CLKPSR = 0; // 0000 (1)
-	
-	// Internal 128 kHz Oscillator (WDT Oscillator)
-	//CLKMSR |= (1<<CLKMS0);
-	
-	DDRB |= (1 << PINB0);
-	
-	TCCR0A |= (1 << COM0A1) | (1 << COM0A0) | (1 << WGM00);
-	TCCR0B |= (1 << CS01) | (1 << WGM02);
-	OCR0A = 0;
-	for (uint8_t i=0; i<0xFF; i++)
-	{
-		_delay_us(2250);
-		OCR0A = i;
-	}
-	OCR0A = 0xFF;
-	
-    /* Replace with your application code */
-    while (1) 
+    DDRB |= (1 << PINB0);
+
+    TCCR0A |= (1 << COM0A1) | (1 << COM0A0) | (1 << WGM00);
+    TCCR0B |= (1 << CS01) | (1 << WGM02);
+    OCR0A = 0;
+    for (uint8_t i=0; i<0xFF; i++)
+    {
+        _delay_us(2250);
+        OCR0A = i;
+    }
+    OCR0A = 0xFF;
+
+    while (1)
     {
     }
 }
-
