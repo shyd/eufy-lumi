6729b790b1be8af11b4c5e2bce157b78d95d0baa
Dennis Schütt <dennis@shyd.de>
2018-02-22 19:54:13 +0100

    added comments

--- fader/main.c
+++ fader/main.c
@@ -13,10 +13,17 @@
 
 int main(void)
 {
+    /* OC0A as output */
     DDRB |= (1 << PINB0);
 
+    /* Set OC0A/OC0B on Compare Match when up-counting.
+    Clear OC0A/OC0B on Compare Match when down-counting.
+    Mode 5: Fast PWM, 8-bit (with WGM02) */
     TCCR0A |= (1 << COM0A1) | (1 << COM0A0) | (1 << WGM00);
+
+    /* Prescaler clk/8 */
     TCCR0B |= (1 << CS01) | (1 << WGM02);
+
     OCR0A = 0;
     for (uint8_t i=0; i<0xFF; i++)
     {
